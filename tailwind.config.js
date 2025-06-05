ed6d25c94a50b321122322f70b1094082033fd0b
crcristian97 <crcristian97.cc@gmail.com>
2025-06-05 09:44:20 -0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#1A2F45',
+        secondary: '#CBA240',
+        'text-primary': '#FFFFFF',
+        'text-secondary': '#B0B9C2',
+      },
+    },
+  },
+  plugins: [],
+} 
