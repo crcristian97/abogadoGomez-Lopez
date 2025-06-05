e683267b612c25b56f11a1b9645e591d251c1f85
crcristian97 <crcristian97.cc@gmail.com>
2025-06-05 10:15:13 -0300

    navbar

--- app/page.js
+++ app/page.js
@@ -1,9 +1,9 @@
+import Navbar from "./components/Navbar";
+
 export default function Home() {
   return (
     <div>
-      <h1 className="font-merriweather font-bold">Bold Heading</h1>
-      <p className="font-merriweather font-light">Light text</p>
-      <p className="font-lato font-light">Light text</p>
+      <Navbar />
     </div>
   );
 }
