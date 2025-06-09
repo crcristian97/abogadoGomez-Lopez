40b6cf3dd8e4a9982e69e4335b00287245e42cfe
crcristian97 <crcristian97.cc@gmail.com>
2025-06-09 18:35:18 -0300

    finish

--- app/blog/page.js
+++ app/blog/page.js
@@ -8,7 +8,7 @@ export default function BlogPage() {
     <div>
       <HeroServices
         src="https://res.cloudinary.com/dgzi8i2ji/image/upload/blog-juridico-en-buenos-aires.webp"
-        alt="Blog de Estudio López Gómez"
+        alt="Blog de Estudio Gómez&López"
         title="Blog Jurídico en Buenos Aires"
         description="Artículos, novedades y análisis legales de nuestros abogados expertos. Mantente informado sobre derecho penal, civil, laboral y más."
         buttonShow={false}
